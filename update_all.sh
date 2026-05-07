3d0b3183936b9a1fc52ede7ca8c98a055865a472
wangmc <w91g@qq.com>
2026-05-07 08:50:57 +0800

    fix

--- update_all.sh
+++ update_all.sh
@@ -1,8 +1,8 @@
-sudo chmod 777 -R debs
+sudo chmod -R 777 ./debs
 sudo ./update.sh
 cd rootless
-sudo chmod 777 -R debs
+sudo chmod -R 777 ./debs
 sudo ./update.sh
 cd ../roothide
-sudo chmod 777 -R debs
+sudo chmod -R 777 ./debs
 sudo ./update.sh
